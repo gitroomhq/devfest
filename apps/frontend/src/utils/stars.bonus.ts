e07eacffc312cd5bef6f96fb892de2ca20ef68d7
Nevo David <nevo@notu.co>
2024-10-04 17:20:11 +0700

    feat: copilotkit

--- apps/frontend/src/utils/stars.bonus.ts
+++ apps/frontend/src/utils/stars.bonus.ts
@@ -1,1 +1,1 @@
-export const starsBonus = ['gitroomhq/postiz-app', 'copilotkit/copilotkit'] as string[];
+export const starsBonus = ['gitroomhq/postiz-app', 'copilotkit/copilotkit', 'https://github.com/ComposioHQ/composio'] as string[];
