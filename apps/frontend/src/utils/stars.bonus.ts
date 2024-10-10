0f865b170e8e8ea002d7bd01a9a9f5465849305d
Nevo David <nevo@notu.co>
2024-10-10 17:42:13 +0700

    feat: taipy

--- apps/frontend/src/utils/stars.bonus.ts
+++ apps/frontend/src/utils/stars.bonus.ts
@@ -1,2 +1,2 @@
-export const starsBonus = ['gitroomhq/postiz-app', 'copilotkit/copilotkit', 'ComposioHQ/composio', 'julep-ai/julep'] as string[];
+export const starsBonus = ['gitroomhq/postiz-app', 'copilotkit/copilotkit', 'ComposioHQ/composio', 'julep-ai/julep', 'avaiga/taipy'] as string[];
 export const frozen = ['copilotkit/copilotkit'];
