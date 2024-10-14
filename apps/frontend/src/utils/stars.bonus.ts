b6135f078e5b541e7ebabc5509a8586148ece169
Nevo David <nevo@notu.co>
2024-10-14 17:57:06 +0700

    feat: llmware

--- apps/frontend/src/utils/stars.bonus.ts
+++ apps/frontend/src/utils/stars.bonus.ts
@@ -1,2 +1,2 @@
-export const starsBonus = ['gitroomhq/postiz-app', 'copilotkit/copilotkit', 'ComposioHQ/composio', 'julep-ai/julep', 'avaiga/taipy'] as string[];
+export const starsBonus = ['gitroomhq/postiz-app', 'copilotkit/copilotkit', 'ComposioHQ/composio', 'julep-ai/julep', 'avaiga/taipy', 'llmware-ai/llmware'] as string[];
 export const frozen = ['copilotkit/copilotkit'];
