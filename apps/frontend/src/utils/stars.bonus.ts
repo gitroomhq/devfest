5919771498c61244cabcd82ad04101264aee8468
Nevo David <nevo@notu.co>
2024-10-17 17:22:38 +0700

    feat: classiq

--- apps/frontend/src/utils/stars.bonus.ts
+++ apps/frontend/src/utils/stars.bonus.ts
@@ -1,2 +1,10 @@
-export const starsBonus = ['gitroomhq/postiz-app', 'copilotkit/copilotkit', 'ComposioHQ/composio', 'julep-ai/julep', 'avaiga/taipy', 'llmware-ai/llmware'] as string[];
+export const starsBonus = [
+  'gitroomhq/postiz-app',
+  'copilotkit/copilotkit',
+  'ComposioHQ/composio',
+  'julep-ai/julep',
+  'avaiga/taipy',
+  'llmware-ai/llmware',
+  'Classiq/classiq-library',
+] as string[];
 export const frozen = ['copilotkit/copilotkit'];
